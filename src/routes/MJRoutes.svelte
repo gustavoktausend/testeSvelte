998a63bb013682fa4b39e9a6828fc1525056bedf
Felipe Silvestri <felipe.silvestri@fontec.inf.br>
2019-12-20 14:37:13 -0300

    geracao de rotas dinamicas para todos os idiomas

--- src/routes/MJRoutes.svelte
+++ src/routes/MJRoutes.svelte
@@ -8,18 +8,20 @@
     import { _ } from 'svelte-i18n';
 
 
-    
-
-
+    import pt_BR from '../../messages/pt-BR';
+    import en_US from '../../messages/en-US';
+    let bundles = [pt_BR.page.url, en_US.page.url];
 
 </script>
 
 <div class="container-fluid">
-    <Route path="{$_('page.url.nav.games')}" component="{Jogos}" />
-    <Route path="{$_('page.url.nav.game')}/:nome" component="{JogosInfo}"  />
-    <Route path="{$_('page.url.nav.game')}/:nome/{$_('page.url.nav.rules')}" component="{JogoRegras}" />
-    <Route path="{$_('page.url.nav.about')}" component="{Sobre}" />
-    <Route path="/" component="{Home}" />
+    {#each bundles as bundle}
+        <Route path="{bundle.nav.games}" component="{Jogos}" />
+        <Route path="{bundle.nav.game}/:nome" component="{JogosInfo}"  />
+        <Route path="{bundle.nav.game}/:nome/{bundle.nav.rules}" component="{JogoRegras}" />
+        <Route path="{bundle.nav.about}" component="{Sobre}" />
+        <Route path="/" component="{Home}" />
+    {/each}
 </div>
 <!--    {$_('page.jogos.title')}-->
 <!--to="/{$_(`page.url.${jogo.nome}`)}"-->
