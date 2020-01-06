e6830a148da93e247cfee5962d04df9a74ff389d
Gustavo Kring <gustavoktausend@gmail.com>
2020-01-06 11:02:03 -0300

    removendo rota Jogos

--- src/routes/MJRoutes.svelte
+++ src/routes/MJRoutes.svelte
@@ -1,7 +1,6 @@
 <script>
     import { Route } from "svelte-routing";
     import Home from "./pages/Home.svelte";
-    import Jogos from "./pages/Jogos.svelte";
     import Sobre from "./pages/Sobre.svelte";
     import JogosInfo from "./pages/JogoInfo.svelte";
     import JogoRegras from "./pages/JogoRegras.svelte";
@@ -15,7 +14,6 @@
 
 <div class="container-fluid">
     {#each bundles as bundle}
-        <Route path="{bundle.nav.games}" component="{Jogos}" />
         <Route path="{bundle.nav.game}/:nome" component="{JogosInfo}"  />
         <Route path="{bundle.nav.game}/:nome/{bundle.nav.rules}" component="{JogoRegras}" />
         <Route path="{bundle.nav.about}" component="{Sobre}" />
